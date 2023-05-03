68ba526139b8cbf8065f9c86c158b67ae45578f2
prselva <131126251+prselva@users.noreply.github.com>
2023-05-03 18:15:30 +0530

    Add files via upload

--- web.sh
+++ web.sh
@@ -3,5 +3,5 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/avail                                                                                                             ability-zone)
-echo "<h1>Hello World from $(hostname -f) in AZ $EC2_AVAIL_ZONE </h1>" > /var/ww                                                                                                             w/html/index.html
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "<h1>Hello World from $(hostname -f) in AZ $EC2_AVAIL_ZONE </h1>" > /var/www/html/index.html
